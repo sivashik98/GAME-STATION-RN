1e5324fdbc46fea41eed8c12c40feb985ca16481
sivashik98 <shelyulev@bk.ru>
2024-05-03 18:52:50 +0300

    Removed excessive code

--- App.tsx
+++ App.tsx
@@ -1,24 +1,14 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
-import {useState} from 'react';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from "@react-navigation/stack";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 
-
-import {Menu} from "./src/pages/Menu";
 import {RootNavigation} from "./src/navigation/Root";
 
 const InnerApp = () => {
-    const [ready, setReady] = useState(true);
 
     return (
         <RootNavigation/>
-        
     );
 }
-
 
 export default function App() {
     return (
